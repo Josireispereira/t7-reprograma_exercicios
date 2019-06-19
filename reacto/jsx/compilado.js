a2d4fbe95e8dad4d48b6734e70e009d0432de2fc
josireispereira <josireispereira@gmail.com>
2019-06-19 10:01:43 -0300

    apagando pasta duplicada

--- reacto/jsx/compilado.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var app = document.getElementById('app'); // const template = `<h1>React no Reprograma, na ${a}</h1>`
-
-var template = React.createElement("h2", null, "aaaaaaaaa"); // dois parametros, o que vai renderizar e onde, por exemplo elemento e container
-
-ReactDOM.render(template, app); // ele não funciona com o template direto, precisamos compilar ele primeiro, com Babel
--- reacto/jsx/script.js
+++
@@ -1,7 +0,0 @@
-const app = document.getElementById('app');
-
-// const template = `<h1>React no Reprograma, na ${a}</h1>`
-var template = <h2>aaaaaaaaa</h2>
-// dois parametros, o que vai renderizar e onde, por exemplo elemento e container
-ReactDOM.render(template, app)
-// ele não funciona com o template direto, precisamos compilar ele primeiro, com Babel
